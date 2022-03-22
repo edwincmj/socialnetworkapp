0c1bce6f5b8c3959416bcc208b1033667aafd307
yrtimd <yrtimdnimiz@gmail.com>
2022-03-22 19:37:15 -0400

    Changed schema

--- skeleton/pa1-schema-example.sql
+++ skeleton/pa1-schema-example.sql
@@ -18,11 +18,12 @@ CREATE TABLE Users(
  user_id1 INTEGER,
  user_id2 INTEGER,
  PRIMARY KEY (user_id1, user_id2),
- FOREIGN KEY (user_id1)
- REFERENCES Users(user_id),
- FOREIGN KEY (user_id2)
- REFERENCES Users(user_id)
- ON DELETE CASCADE
+ CONSTRAINT friend_user1_fk
+     FOREIGN KEY (user_id1)
+     REFERENCES Users(user_id) ON DELETE CASCADE,
+ CONSTRAINT friend_user2_fk
+     FOREIGN KEY (user_id2)
+     REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Albums(
@@ -31,9 +32,9 @@ CREATE TABLE Albums(
  date DATE,
  user_id INTEGER NOT NULL,
  PRIMARY KEY (albums_id),
- FOREIGN KEY (user_id)
- REFERENCES Users(user_id)
- ON DELETE CASCADE
+ CONSTRAINT albums_user_fk
+	FOREIGN KEY (user_id)
+	REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Tags(
@@ -49,9 +50,9 @@ CREATE TABLE Photos(
  albums_id INTEGER NOT NULL,
  user_id INTEGER NOT NULL,
  PRIMARY KEY (photo_id),
- CONSTRAINT album_fk
+ CONSTRAINT photos_album_fk
     FOREIGN KEY (albums_id) REFERENCES Albums (albums_id) ON DELETE CASCADE,
- CONSTRAINT user_fk
+ CONSTRAINT photos_user_fk
     FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE
 );
 
@@ -59,11 +60,12 @@ CREATE TABLE Tagged(
  photo_id INTEGER,
  tag_id INTEGER,
  PRIMARY KEY (photo_id, tag_id),
- FOREIGN KEY(photo_id)
- REFERENCES Photos (photo_id),
- FOREIGN KEY(tag_id)
- REFERENCES Tags (tag_id)
- ON DELETE CASCADE
+ CONSTRAINT tagged_photo_fk
+     FOREIGN KEY(photo_id)
+     REFERENCES Photos (photo_id) ON DELETE CASCADE,
+ CONSTRAINT tagged_tag_fk 
+    FOREIGN KEY(tag_id)
+    REFERENCES Tags (tag_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Comments(
@@ -73,22 +75,24 @@ CREATE TABLE Comments(
  text VARCHAR (100),
  date DATE,
  PRIMARY KEY (comment_id),
- FOREIGN KEY (user_id)
- REFERENCES Users (user_id),
- FOREIGN KEY (photo_id)
- REFERENCES Photos (photo_id)
- ON DELETE CASCADE
+ CONSTRAINT comments_user_fk
+    FOREIGN KEY (user_id)
+    REFERENCES Users (user_id) ON DELETE CASCADE,
+ CONSTRAINT comments_photo_fk
+     FOREIGN KEY (photo_id)
+     REFERENCES Photos (photo_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Likes(
  photo_id INTEGER,
  user_id INTEGER,
  PRIMARY KEY (photo_id,user_id),
- FOREIGN KEY (photo_id)
- REFERENCES Photos (photo_id),
- FOREIGN KEY (user_id)
- REFERENCES Users (user_id)
- ON DELETE CASCADE
+ CONSTRAINT likes_photo_fk
+    FOREIGN KEY (photo_id)
+    REFERENCES Photos (photo_id),
+CONSTRAINT likes_user_fk
+     FOREIGN KEY (user_id)
+     REFERENCES Users (user_id) ON DELETE CASCADE
 );
 
 INSERT INTO users(user_id,email,first_name,last_name,password) VALUES (1,'testing@gmail.com','John','Smith','password');
